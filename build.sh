e2de606d104dd7dd60d694c20839c62bc488f6f0
Peter Humke <humkepm@gmail.com>
2016-03-20 21:35:05 -0700

    Updated the build to have a couple of common functions for generating the tar paths, building, installing, and testing the packages given just a name and a version number.  Used this file to build and install everything locally one at a time.  Haven't tried uploading to anaconda.org yet.

--- build.sh
+++ build.sh
@@ -1,19 +1,64 @@
 # Following the instructions found here:
 # http://conda.pydata.org/docs/build_tutorials/pkgs2.html
 
-Root_Path=~/anaconda3/conda-bld/
+set -e
 
-build_chartkick() {
-    conda build chartkick
+root_path=~/anaconda3/conda-bld/
+python_vers=py35_0
+os=linux-64
 
-    conda convert --platform all $Root_Path/linux-64/chartkick-0.4.2-py35_0.tar.bz2 -o $Root_Path
+get_tar_path() {
+    package=${1}
+    version=${2}
 
-    # force a reinstall to as a validation test
-    conda uninstall chartkick --yes
-    conda install --use-local --yes chartkick
+    tar_path=$root_path/$os/$package-$version-$python_vers.tar.bz2
 }
 
-build_chartkick
+build() {
+    package=${1}
+    version=${2}
+
+    conda build $package
+
+    get_tar_path $package $version
+
+    conda convert --platform all $tar_path -o $root_path
+
+    # force a reinstall to as a validation test
+    # only uninstall if it exists - ugly hack as grep returns an error if nothing matched
+    set +e
+    conda list | grep $package | grep $version
+    found=$?
+    set -e
+
+    if [ "$found" -eq 0 ]; then
+        conda uninstall --yes $package=$version
+    fi
+
+    conda install --use-local --yes $package=$version
+}
+
+# Build order was determined by resolving the dependency tree prior to need, then in alphabetical order
+
+# alembic dependencies
+# build python-editor 0.5
+
+# flask-admin dependencies
+# build wtforms 2.1.0
+
+# Needed by airflow
+# build alembic 0.8.5
+# build babel 1.3
+# build chartkick 0.4.2
+# build croniter 0.3.12
+# build dill 0.2.5
+# build flask-admin 1.4.0
+# build flask-cache 0.13.1
+# build gunicorn 19.3.0
+# build setproctitle 1.1.9
+# build thrift 0.9.3
+build flask-wtf 0.12
+
 
 # anaconda login
-# anaconda upload $Root_Path/linux-64/*
+# anaconda upload $root_path/linux-64/*
